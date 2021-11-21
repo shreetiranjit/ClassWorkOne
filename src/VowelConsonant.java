fa9ad594677ca3a9cbf6caca399157d9d0ec2d86
shreetiranjit <ranjit.shreeti1@gmail.com>
2021-11-21 10:17:45 +0545

    Intial Commit

--- src/VowelConsonant.java
+++ src/VowelConsonant.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class VowelConsonant {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        // Data representation
+
+        System.out.print("Enter a character :");
+
+        char input_char = sc.nextLine().charAt(0);
+
+        if (input_char == 'a' || input_char == 'e' || input_char == 'i'  || input_char == 'o' || input_char == 'u' || input_char == 'A' || input_char == 'E' || input_char == 'I' || input_char == 'O' || input_char == 'U') {
+            System.out.print("This is a vowel");
+        }
+        else{
+            System.out.print("This is not a vowel");
+        }
+    }
+
+
+
 }
